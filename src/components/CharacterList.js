fffd52fdc201d9ebe7662575a719900f5b6ec7fd
mykenzie <mykenzierogers@gmail.com>
2019-06-12 08:03:49 -0500

    initial commit

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,26 +1,8 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import axios from "axios";
-import { setLoading, getCharacters } from "../ducks/breakingBad";
-import { setStarWarsLoading, getStarWarsCharacters } from "../ducks/starWars";
 
 class CharacterList extends Component {
   componentDidMount() {
-    this.props.getCharacters();
-    this.props.getStarWarsCharacters();
-    // .then(response => {
-    //   this.setState({
-    //     breakingBad: response.data
-    //   });
-    //   this.props.setLoading(false);
-    // });
 
-    // .then(response => {
-    //   this.setState({
-    //     starWars: response.data.results
-    //   });
-    //   this.props.setStarWarsLoading(false);
-    // });
   }
 
   render() {
@@ -35,34 +17,17 @@ class CharacterList extends Component {
         <aside>
           <h1>Breaking Bad Characters</h1>
           <div>
-            {this.props.breakingBad.error}
-            {this.props.breakingBad.loading && "Loading..."}
-            {this.props.breakingBad.characters.map(character => (
-              <p key={character.char_id}>{character.name}</p>
-            ))}
+            {/* Breaking Bad Characters Here */}
           </div>
         </aside>
         <aside>
           <h1>Star Wars Characters</h1>
           <div>
-            {this.props.starWars.loading && "Loading..."}
-            {this.props.starWars.characters.map(character => {
-              return <p key={character.name}>{character.name}</p>;
-            })}
+            {/* Star Wars Characters Here */}
           </div>
         </aside>
       </div>
     );
   }
 }
-const mapStateToProps = reduxState => {
-  console.log(reduxState);
-  return {
-    breakingBad: reduxState.breakingBad,
-    starWars: reduxState.starWars
-  };
-};
-export default connect(
-  mapStateToProps,
-  { setLoading, setStarWarsLoading, getCharacters, getStarWarsCharacters }
-)(CharacterList);
+export default CharacterList;
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import promise from "redux-promise-middleware";
-import breakingBad from "./ducks/breakingBad";
-import starWars from "./ducks/starWars";
-
-const rootReducer = combineReducers({ breakingBad: breakingBad, starWars });
-const store = createStore(rootReducer, applyMiddleware(promise));
-
-export default store;
