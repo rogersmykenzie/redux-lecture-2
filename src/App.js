615dd5ecb22f64d0338676b3b92b4c82f4eacfd2
mykenzie <mykenzierogers@gmail.com>
2019-06-12 09:44:51 -0500

    reset project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-import { Provider } from "react-redux";
 import CharacterList from "./components/CharacterList";
-import store from "./store";
 import "./App.css";
 
 function App() {
   return (
-    <Provider store={store}>
-      {/* <Router> */}
       <div className="App">
         <CharacterList />
       </div>
-      {/* </Router> */}
-    </Provider>
   );
 }
 
