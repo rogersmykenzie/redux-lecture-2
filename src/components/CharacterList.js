6cc4c128032ceef41a7277e45bdd35d44a0229d9
Eric Sellors <4157996+esellors@users.noreply.github.com>
2020-02-19 14:29:46 -0600

    Added notes in CharacterList at bottom

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -17,28 +17,28 @@ class CharacterList extends Component {
           justifyContent: "space-evenly"
         }}
       >
-        <aside>
+        <section>
           <h1>Rick & Morty Characters</h1>
           <div>
             {/* Rick & Morty Characters Here */}
             {
               this.props.rickMorty.loading
-                ? 'Loading...'
+                ? <img src='https://resources.humandx.org/static/img/loading_spinner.gif' alt='Loading..' />
                 : this.props.rickMorty.error || (this.props.rickMorty.characters.results && this.props.rickMorty.characters.results.map((val, i) => <p key={`bb${i}`}>{val.name}</p>))
             }
           </div>
-        </aside>
-        <aside>
+        </section>
+        <section>
           <h1>Star Wars Characters</h1>
           <div>
             {/* Star Wars Characters Here */}
             {
               this.props.starWars.loading
-                ? 'Loading...'
+                ? <img src='https://resources.humandx.org/static/img/loading_spinner.gif' alt='Loading..' />
                 : this.props.starWars.error || this.props.starWars.characters.map((val, i) => <p key={`sw${i}`}>{val.name}</p>)
             }
           </div>
-        </aside>
+        </section>
       </div>
     );
   }
@@ -56,3 +56,11 @@ export default connect(mapStateToProps,
     getRickMortyCharacters,
     getStarWarsCharacters
   })(CharacterList);
+
+// If we didn't need to use mapStateToProps, we still need something in that spot since our object with the functions we needed MUST be in the spot of the second argument. If this were the case, our export would look like the following using null as a fill-in:
+
+// export default connect(null, 
+//   {
+//     getRickMortyCharacters,
+//     getStarWarsCharacters
+//   })(CharacterList);
